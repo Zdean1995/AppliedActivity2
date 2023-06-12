4cf067140b65f622700fca3e27c91ef014e31491
Zachary <zdean1995@gmail.com>
2023-06-12 18:53:54 -0400

    Add project files.

---
+++ CanadianHolidayChallenge/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CanadianHolidayChallenge;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ CanadianHolidayChallenge/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace CanadianHolidayChallenge;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
